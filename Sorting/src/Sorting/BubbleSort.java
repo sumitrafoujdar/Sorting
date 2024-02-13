4f30f9763cdf32b512f1abc2bb84654111617263
sumitrafoujdar <sumitrafoujdar@localhost>
2024-02-14 01:04:22 +0530

    Bubble Sort

---
+++ Sorting/src/Sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {78, 55, 45, 98, 12, 47, 65, 32};
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
